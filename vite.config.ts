1d888676d29a0da2c4c9f8380f5b8992921a71bb
TeddySnowden <takacsgyul@gmail.com>
2026-02-16 13:36:56 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/meetfix/',
+  base: '/MeetFix/',
   plugins: [react()],
   resolve: {
     alias: {
